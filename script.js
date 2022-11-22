3a9af7a06b182f4c4be6880999b4f1eb163ecb31
Moksleivis <Moksleivis@kitm.local>
2022-11-22 11:49:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const create = document.getElementById('sukurk');
+const rezult = document.getElementById('rezultatas');
+
+create.addEventListener("click", photoCreater);
+
+let img =  [
+[1, 'src', 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'],
+[2, 'src', 'https://images.pexels.com/photos/1695052/pexels-photo-1695052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'],
+[3, 'src', 'https://images.pexels.com/photos/428310/pexels-photo-428310.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'],
+[4, 'src', 'https://images.pexels.com/photos/129207/pexels-photo-129207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'],
+[5, 'src', 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'],
+[6, 'src', 'https://images.pexels.com/photos/773958/pexels-photo-773958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1']
+];
+
+
+function photoCreater(){
+
+    let iR = Math.random();
+
+}
