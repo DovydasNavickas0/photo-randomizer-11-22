407acb4262b4c13dcdb8de0896cd0f413da5ab29
D.Navickas <112947625+DovydasNavickas0@users.noreply.github.com>
2022-11-23 20:53:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const create = document.getElementById('sukurk');
 const rezult = document.getElementById('rezultatas');
+const buttonDiv = document.getElementById('buttonDiv')
 
 create.addEventListener("click", photoCreater);
 
+function shuffle(array) {
+    var i = array.length,
+        j = 0,
+        temp;
+
+    while (i--) {
+
+        j = Math.floor(Math.random() * (i+1));
+
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    return array;
+}
 
 function photoCreater(){
+
+    rezult.style.visibility = "visible";
+
+    let j = 1
+
+    let randomizer = shuffle([0, 1, 2, 3, 4, 5])
 
     const img =  [
         ['https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'],
@@ -18,28 +43,31 @@ function photoCreater(){
 
     for(let i = 0; i<img.length; i++){
 
-        const random = Math.floor(Math.random() * img.length)
+        console.log(randomizer[j])
 
-        console.log(img[random])
+        console.log(img[randomizer[j]])
         let imgTable = document.createElement('img');
-        imgTable.setAttribute('src', img[random]);
+        imgTable.setAttribute('src', img[randomizer[j]]);
         imgTable.setAttribute('height', 150);
         imgTable.setAttribute('width', 300);
         rezult.appendChild(imgTable)
+
+        j += 1
+
     }
 
-    create.style.visibility = "hidden"
-    let nerodyti = document.createElement('button')
-    nerodyti.value = "nerodyti"
-//    let  = document.createElement('button')
-//    nerodyti.value = "nerodyti"
-create.addEventListener("click", photoCreater);
+    create.style.visibility = "hidden";
+    const nerodyti = document.createElement('button');
+    nerodyti.textContent = "nerodyti";
 
-    body.appendChild(nerodyti)
+    buttonDiv.appendChild(nerodyti);
 
+    nerodyti.addEventListener("click", hide);
 
-}
+    function hide(){
+        rezult.style.visibility = "hidden";
+        create.style.visibility = "visible";
+        buttonDiv.parentNode.removeChild(nerodyti);
+    }
 
-function hide(){
-    rezult.style.visibility = "hidden";
 }
